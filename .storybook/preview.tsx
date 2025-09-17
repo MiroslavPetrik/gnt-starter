cb7ca8770a0f1a07166263520484d3fd058fbe9c
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-09-17 15:38:47 +0200

    feat(storybook): upgrade

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/nextjs";
 import { FlowbiteTheme } from "../src/app/_components/flowbite-theme";
 import "../src/styles/globals.css";
 
