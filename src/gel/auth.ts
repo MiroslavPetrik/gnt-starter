25326ab4b6952b13fb64cd881c3658f06473cb4d
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-09-30 13:54:03 +0200

    fix(i18n): gel errors

--- src/gel/auth.ts
+++ src/gel/auth.ts
@@ -52,10 +52,10 @@ export const authAction = formAction
   })
   .error(async ({ error, ctx: { t } }) => {
     if (error instanceof Error) {
-      const dbError = readDbError(error, t);
+      const gelError = translateGelError(error, t);
 
       return {
-        message: dbError?.message ?? getErrorMessage(error, t),
+        message: gelError?.message ?? getErrorMessage(error, t),
       };
     } else {
       return {
@@ -72,50 +72,22 @@ const getErrorMessage = (error: Error, t: (key: string) => string) => {
   return error.message ?? t("auth:unexpectedError");
 };
 
-const readDbError = (originalError: Error, t: (key: string) => string) => {
-  try {
-    const { error } = stringToDBError.parse(originalError.message);
+const translateGelError = (error: Error, t: (key: string) => string) => {
+  // https://github.com/geldata/gel/blob/6b29802935d71545e242e07db7a4a2074753287c/edb/server/protocol/auth_ext/errors.py#L183
+  if (error.message === "Email verification is required") {
+    return { message: t("auth:gel.emailVerificationRequired") };
+  }
 
-    // https://github.com/edgedb/edgedb/blob/6b29802935d71545e242e07db7a4a2074753287c/edb/server/protocol/auth_ext/errors.py#L179
-    if (error.message === "Email verification is required") {
-      return { ...error, message: t("auth:edgedb.emailVerificationRequired") };
-    }
+  // https://github.com/geldata/gel/blob/e675e03b22dc040812ef506fd30845e2d0a7f386/edb/server/protocol/auth_ext/errors.py#L102
+  if (
+    error.message ===
+    "Could not find an Identity matching the provided credentials"
+  ) {
+    return { message: t("auth:gel.noIdentityFound") };
+  }
 
-    // https://github.com/edgedb/edgedb/blob/639c91e9207275c114828556a1b00c4a3029d8c1/edb/server/protocol/auth_ext/http.py#L169
-    if (error.message === "No identity found") {
-      return { ...error, message: t("auth:edgedb.noIdentityFound") };
-    }
-
-    // https://github.com/edgedb/edgedb/blob/6b29802935d71545e242e07db7a4a2074753287c/edb/server/protocol/auth_ext/errors.py#L123
-    if (error.message === "This user has already been registered") {
-      return { ...error, message: t("auth:edgedb.userAlreadyRegistered") };
-    }
-
-    return error;
-  } catch {
-    return null;
+  // https://github.com/geldata/gel/blob/e675e03b22dc040812ef506fd30845e2d0a7f386/edb/server/protocol/auth_ext/errors.py#L123
+  if (error.message === "This user has already been registered") {
+    return { message: t("auth:gel.userAlreadyRegistered") };
   }
 };
-
-const dbAuthError = z.object({
-  error: z.object({
-    type: z.string(),
-    message: z.string(),
-  }),
-});
-
-const stringToDBError = z
-  .string()
-  .transform((str, ctx): z.infer<typeof dbAuthError> => {
-    try {
-      const err: unknown = JSON.parse(str);
-
-      return dbAuthError.parse(err);
-    } catch {
-      ctx.addIssue({
-        code: "custom",
-        message: "The string does not contain DB auth error",
-      });
-      return z.NEVER;
-    }
-  });
