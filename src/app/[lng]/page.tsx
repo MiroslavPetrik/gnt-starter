a72a58c1fcb0c964ff34378e33086c9a019c1abc
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-11-28 12:12:52 +0100

    fix: auth layout group

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -16,29 +16,27 @@ export default async function Home({ params }: Params) {
   }
 
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-r from-green-400 to-cyan-600 text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            <span>GNT</span> Starter
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="rounded-xl bg-white/10 p-4 text-center hover:bg-white/20"
-              href="/signin"
-            >
-              <h3 className="text-2xl font-bold">{t("signIn")}</h3>
-            </Link>
-            <Link
-              className="rounded-xl bg-white/10 p-4 text-center hover:bg-white/20"
-              href="/signup"
-            >
-              <h3 className="text-2xl font-bold">{t("signUp")}</h3>
-            </Link>
-          </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-r from-green-400 to-cyan-600 text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <span>GNT</span> Starter
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <Link
+            className="rounded-xl bg-white/10 p-4 text-center hover:bg-white/20"
+            href="/signin"
+          >
+            <h3 className="text-2xl font-bold">{t("signIn")}</h3>
+          </Link>
+          <Link
+            className="rounded-xl bg-white/10 p-4 text-center hover:bg-white/20"
+            href="/signup"
+          >
+            <h3 className="text-2xl font-bold">{t("signUp")}</h3>
+          </Link>
         </div>
-        <LanguageSwitcher lng={lng} />
-      </main>
-    </>
+      </div>
+      <LanguageSwitcher lng={lng} />
+    </main>
   );
 }
